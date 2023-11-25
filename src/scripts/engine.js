9368f8105babcefa0a0e696689b4837857d156e3
Renan Menezes <2003rcsm@gmail.com>
2023-11-25 19:14:32 -0300

    Inserindo Efeitos sonoros

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -103,7 +103,7 @@ async function setCardsField(cardId) {
 }
 
 async function drawButton(text) {
-    state.actions.button.innerText = text;
+    state.actions.button.innerText = text.toUpperCase();
     state.actions.button.style.display = "block";
 }
 
@@ -112,19 +112,21 @@ async function updateScore() {
 }
 
 async function checkDuelResults(playerCardId, computerCardId){
-    let duelResults = "Empate";
+    let duelResults = "Draw";
     let playerCard = cardData[playerCardId];
 
 
     if (playerCard.WinOf.includes(computerCardId)){
-        duelResults = "Ganhou"
+        duelResults = "Win";
         state.score.playerScore++;
     }
 
     if (playerCard.LoseOf.includes(computerCardId)) {
-        duelResults = "Perdeu";
+        duelResults = "Lose";
         state.score.computerScore++;
     }
+
+    await playAudio(duelResults);
 
     return duelResults;
 }
@@ -151,6 +153,24 @@ async function drawCards(cardNumbers, fieldSide) {
 
         document.getElementById(fieldSide).appendChild(cardImage);
     }
+}
+
+async function resetDuel(){
+    state.cardSprites.avatar.src = "";
+    state.actions.button.style.display = "none";
+
+    state.fieldCards.player.style.display = "none";
+    state.fieldCards.computer.style.display = "none";
+
+    init();
+}
+
+async function playAudio(status){
+    const audio = new Audio(`./src/assets/audios/${status}.wav`);
+    
+    try{
+        audio.play();
+    } catch {}
 }
 
 function init() {
